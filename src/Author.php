fe65e4cab207337a336914201f91364a2842da5e
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-19 08:55:09 -0700

    begin build for author class

---
+++ src/Author.php
@@ -0,0 +1,40 @@
+<?php
+
+    class Author
+    {
+        private $author_name;
+        private $id;
+
+        function __construct($author_name, $id = null)
+        {
+            $this->author_name = $author_name;
+            $this->id = $id;
+        }
+
+        function getAuthorName()
+        {
+            return $this->author_name;
+        }
+
+        function setAuthorName($new_author_name)
+        {
+            $this->author_name = $new_author_name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $ececuted = $GLOBALS['DB']->exec("INSERT INTO author (author_name) VALUES ('{$this->getAuthorName}')");
+            if ($executed) {
+                $this->id = $GLOBALS['DB']->lastInsertId();
+                return true;
+            } else {
+                return false;
+            }
+        }   
+    }
+ ?>
