5e3e3acaa1f928711fee9839f67bb5c92d08e961
jacob ruleaux and Michelle Poterek <michelle@>
2017-07-20 10:30:10 -0700

    add find method test to patron class

--- src/Patron.php
+++ src/Patron.php
@@ -37,7 +37,44 @@
             }
         }
 
-        // static function
+        static function getAll()
+        {
+            $returned_patrons = $GLOBALS['DB']->query("SELECT * FROM patrons;");
+            $patrons = array();
+            foreach ($returned_patrons as $patron) {
+                $patron_name = $patron['name'];
+                $id = $patron['id'];
+                $new_patron = new Patron($patron_name, $id);
+                array_push($patrons, $new_patron);
+            }
+            return $patrons;
+        }
+
+        static function deleteAll()
+        {
+            $executed = $GLOBALS['DB']->exec("DELETE FROM patrons;");
+            if ($executed) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        static function find($search_id)
+        {
+            $returned_patrons = $GLOBALS['DB']->prepare("SELECT * FROM patrons WHERE id = :id");
+            $returned_patrons->bindParam(':id', $search_id, PDO::PARAM_STR);
+            $returned_patrons->execute();
+            foreach ($returned_patrons as $patron) {
+              $patron_name = $patron['name'];
+              $id = $patron['id'];
+              if ($id == $search_id) {
+                 $found_patron = new Patron($patron_name, $id);
+              }
+            }
+            return $found_patron;
+        }
+
 
 
     }
--- tests/PatronTest.php
+++ tests/PatronTest.php
@@ -14,6 +14,12 @@
 
     class PatronTest extends PHPUnit_Framework_TestCase
     {
+        protected function tearDown()
+               {
+                 Patron::deleteAll();
+                 Author::deleteAll();
+                 Book::deleteAll();
+               }
 
         function testGetName()
         {
@@ -47,5 +53,55 @@
            //Assert
            $this->assertTrue($executed, "Theres no PATRON in database!!!!");
        }
+
+       function testGetAll()
+       {
+            //Arrange
+            $patron_name = "Poe";
+            $test_name = new Patron($patron_name);
+            $test_name->save();
+
+            $patron_name_2 = "Wells";
+            $test_name_2 = new Patron($patron_name_2);
+            $test_name_2->save();
+            //Act
+            $result = Patron::getAll();
+            //Assert
+            $this->assertEquals([$test_name, $test_name_2], $result);
+        }
+
+        function testDeleteAll()
+        {
+            //Arrange
+           $patron_name = "Poe";
+           $test_name = new Patron($patron_name);
+           $test_name->save();
+
+           $patron_name_2 = "Wells";
+           $test_name_2 = new Patron($patron_name_2);
+           $test_name_2->save();
+
+           //Act
+           Patron::deleteAll();
+           $result = Patron::getAll();
+           //Assert
+           $this->assertEquals([], $result);
+        }
+
+        function testFind()
+        {
+            //Arrange
+            $patron_name = "Joe";
+            $test_name = new Patron($patron_name);
+            $test_name->save();
+
+            $patron_name_2 = "Wlls";
+            $test_name_2 = new Patron($patron_name_2);
+            $test_name_2->save();
+            //Act
+            $result = Patron::find($test_name->getId());
+            //Assert
+            $this->assertEquals($test_name, $result);
+         }
    }
 ?>
